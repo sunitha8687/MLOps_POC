db380d4d6680fbe4504f4383cf51a969a4b78306
SunithaReply <s.radhakrishnan@reply.de>
2023-03-25 17:35:57 +0100

    removed .

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import logging
 import os
 print("print cwd:",os.getcwd())
 logging.info(os.getcwd())
-from preprocess import data_preprocess
+from preprocess import data_preprocess # if running in local: include src.preprocess
 from train import train_model
 from predict import predict_probabilities
 from new_model import predict_evaluate
@@ -39,8 +39,8 @@ def model_logging():
 if __name__ == '__main__':
 
     try:
-        df = pd.read_csv("../data/historical_sensor_data.csv", sep=',')
-        inference_df = pd.read_csv("../data/latest_sensor_data.csv", sep=',')
+        df = pd.read_csv("data/historical_sensor_data.csv", sep=',')  # add double dots ../data/ if running in local
+        inference_df = pd.read_csv("data/latest_sensor_data.csv", sep=',')
     except Exception as e:
         logger.exception("Unable to download training and inference CSV, check your internet connection. Error: %s", e)
 
