8a9dec6928b46c9e266d90735be780a14632c1f3
SunithaReply <s.radhakrishnan@reply.de>
2023-03-25 17:39:24 +0100

    removed . in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ from os.path import abspath
 from os.path import dirname as d
 
 @pytest.fixture()
-def test_data(): # you cant download a model everytime, so need to put that in the fixture
-    yield "../data/historical_sensor_data.csv"
+def test_data():  # you cant download a model everytime, so need to put that in the fixture
+    yield "data/historical_sensor_data.csv"  # add double dots ../data/ if running in local
 
 data_science_problem = f"{d(d(abspath(__file__)))}"
 sys.path.append(f"{data_science_problem}/src")
