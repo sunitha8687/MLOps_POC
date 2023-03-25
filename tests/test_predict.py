dfa35d8cffe16b58585f6052cbc040f0f6adddb5
SunithaReply <s.radhakrishnan@reply.de>
2023-03-25 17:47:45 +0100

    corrected pytest function

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -10,7 +10,7 @@ from src.new_model import predict_evaluate
 def test_predict(test_data):
     inference_df = pd.read_csv(test_data)
     inference_df = inference_df[:500]
-    f1_score, precision, recall = predict_evaluate(inference_df)
+    model, f1_score, precision, recall = predict_evaluate(inference_df)
     # Check returned f1 score value
     rounded_f1 = round(f1_score, 2)
     assert rounded_f1 == 0.96
