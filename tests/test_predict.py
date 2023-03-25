cd5050ca17d371cb01bd3a07fd3fd0961c4226f8
SunithaReply <s.radhakrishnan@reply.de>
2023-03-25 09:44:49 +0100

    changes obj

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -6,10 +6,12 @@ from src.model_code_changes import prediction
 
 
 @pytest.mark.predict
+
 def test_predict(test_data):
+    pred = prediction()
     inference_df = pd.read_csv(test_data)
     inference_df = inference_df[:500]
-    f1_score, accuracy_score, precision, recall = prediction.predict_evaluate(inference_df)
+    f1_score, accuracy_score, precision, recall = pred.predict_evaluate(inference_df)
     # Check returned scores value
     rounded_f1 = round(f1_score, 2)
     assert rounded_f1 == 0.98
